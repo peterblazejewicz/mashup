cf93f0062b209b29cdabbf01f6b0d9a248c301d7
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-05-17 20:58:45 +0200

    Remove unused data-api support (no plan to use it in legacy software)

--- modal.js
+++ modal.js
@@ -316,26 +316,7 @@
     $.fn.modal = old
     return this
   }
-
-
-  // MODAL DATA-API
-  // ==============
-
-  $(document).bind('click.bs.modal.data-api', '[data-toggle="modal"]', function (e) {
-    var $this   = $(this)
-    var href    = $this.attr('href')
-    var $target = $($this.attr('data-target') || (href && href.replace(/.*(?=#[^\s]+$)/, ''))) // strip for ie7
-    var option  = $target.data('bs.modal') ? 'toggle' : $.extend({ remote: !/#/.test(href) && href }, $target.data(), $this.data())
-
-    if ($this.is('a')) e.preventDefault()
-
-    $target.one('show.bs.modal', function (showEvent) {
-      if (showEvent.isDefaultPrevented()) return // only register focus restorer if modal will actually get shown
-      $target.one('hidden.bs.modal', function () {
-        $this.is(':visible') && $this.trigger('focus')
-      })
-    })
-    Plugin.call($target, option, this)
-  })
+  
+  // no init via data-api
 
 }(jQuery);
